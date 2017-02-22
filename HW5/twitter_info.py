e01757e862229957f01e62450f4dc20c44e10a7b
liggs6 <liggiob@umich.edu>
2017-02-21 23:28:21 -0500

    Add files via upload

---
+++ HW5/twitter_info.py
@@ -0,0 +1,4 @@
+consumer_key = "Y8HDmKK8Cwn5QgIXZYMVpgCvh" 
+consumer_secret = "Fqme3VclBo3sfZfX7aTT3tt64NRLSfKGsvDySbm1HROjG91k28"
+access_token = "785226100351836160-hpdJjMe56QfTOLd48baiB8jaY536CDF"
+access_token_secret = "LBF15cFTgg3gexOxEj4WJbL99IFn8Z0AiVTZ7utPiZx7S"
